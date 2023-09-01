815d7263f49b1aa4dd40cd980f3f30efc61f58c8
Fatih Yanık <69479457+YanikFatih@users.noreply.github.com>
2023-09-01 17:43:05 +0300

    homepage design changed

--- lib/pages/home.dart
+++ lib/pages/home.dart
@@ -1,7 +1,8 @@
 import 'dart:io';
 import 'package:flutter/material.dart';
 import 'package:image_picker/image_picker.dart';
-import 'package:image_video_app/pages/addData.dart';
+import 'package:image_video_app/pages/imagePage.dart';
+import 'package:image_video_app/pages/voiceNotePage.dart';
 import 'package:video_player/video_player.dart';
 
 class homePage extends StatefulWidget {
@@ -12,25 +13,6 @@ class homePage extends StatefulWidget {
 }
 
 class _homePageState extends State<homePage> {
-  /*PlatformFile? pickedFile;
-  String? videoUrl;
-  VideoService videoService = VideoService();
-  VideoPlayerController? videoPlayerController;*/
-  File? _video;
-  final picker = ImagePicker();
-  VideoPlayerController? videoPlayerController;
-  pickVideo() async {
-    final video = await picker.pickImage(source: ImageSource.gallery);
-    _video = File(video!.path);
-    videoPlayerController = VideoPlayerController.file(_video!)..initialize().then((_){
-        setState(() {
-
-        });
-        videoPlayerController!.play();
-      }
-    );
-  }
-
   @override
   void dispose() {
     /*videoPlayerController?.dispose();*/
@@ -39,87 +21,102 @@ class _homePageState extends State<homePage> {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      backgroundColor: Colors.grey[300],
       appBar: AppBar(
-        backgroundColor: Colors.grey[200],
-        actions: [
-          PopupMenuButton(
-              icon: Icon(Icons.menu, color: Colors.black,),
-              color: Colors.white,
-              itemBuilder: (context){
-              return [
-                const PopupMenuItem(
-                    child: Text("Photos"),
-                ),
-                const PopupMenuItem(
-                  child: Text("Videos"),
-                ),
-                const PopupMenuItem(
-                  child: Text("Voice Notes"),
-                ),
-              ];
-          })
-        ],
+        backgroundColor: Colors.grey[300],
         title: const Text(
-          'Your Libary',
-           style: TextStyle(
+          "Add to Your Library",
+          style: TextStyle(
             fontWeight: FontWeight.w400,
             fontSize: 20,
-        ),
+          ),
         ),
       ),
       body: Container(
         width: MediaQuery.of(context).size.width,
         height: MediaQuery.of(context).size.height,
-        child: Padding(
-          padding: const EdgeInsets.only(right: 10, left: 10, top: 8),
-          child: ListView.builder(
-            itemCount: 4,
-            itemBuilder:(BuildContext context, index){
-              return ListTile(
-                title: Text("video name"),
-                subtitle: Text("video description"),
-                leading: CircleAvatar(
-                  backgroundColor: Colors.grey[400],
-                ),
-                onTap: (){
-                  //detaylı sayfaya yönlendirme
-                },
-              );
-            }
-          )
+        child: Column(
+          children: [
+            const SizedBox(
+              height: 50,
+            ),
+            InkWell(
+              onTap: (){
+                Navigator.push(context, MaterialPageRoute(builder: (context) => ImagePage()));
+              },
+              child: Container(
+                  width: 120,
+                  height: 120,
+                  decoration: BoxDecoration(
+                    borderRadius: BorderRadius.circular(10),
+                    color: Colors.grey[400],
+                  ),
+                  child: const Padding(
+                    padding: EdgeInsets.all(15.0),
+                    child: Column(
+                      children: [
+                        Icon(Icons.add_photo_alternate, size: 60),
+                        SizedBox(height: 5),
+                        Text('Photo'),
+                      ],
+                    ),
+                  )
+              ),
+            ),
+            const SizedBox(
+              height: 50,
+            ),
+            InkWell(
+              onTap: (){
+
+              },
+              child: Container(
+                  width: 120,
+                  height: 120,
+                  decoration: BoxDecoration(
+                    borderRadius: BorderRadius.circular(10),
+                    color: Colors.grey[400],
+                  ),
+                  child: const Padding(
+                    padding: EdgeInsets.all(15.0),
+                    child: Column(
+                      children: [
+                        Icon(Icons.video_call_rounded, size: 60),
+                        SizedBox(height: 5),
+                        Text('Video'),
+                      ],
+                    ),
+                  )
+              ),
+            ),
+            const SizedBox(
+              height: 50,
+            ),
+            InkWell(
+              onTap: (){
+                Navigator.push(context, MaterialPageRoute(builder: (context) => VoiceNotePage()));
+              },
+              child: Container(
+                  width: 120,
+                  height: 120,
+                  decoration: BoxDecoration(
+                    borderRadius: BorderRadius.circular(10),
+                    color: Colors.grey[400],
+                  ),
+                  child: const Padding(
+                    padding: EdgeInsets.all(15.0),
+                    child: Column(
+                      children: [
+                        Icon(Icons.mic_none_rounded, size: 60),
+                        SizedBox(height: 5),
+                        Text('Voice'),
+                      ],
+                    ),
+                  )
+              ),
+            ),
+          ],
         ),
-      ),
-      floatingActionButton: FloatingActionButton(
-        onPressed: (){
-          Navigator.push(context, MaterialPageRoute(builder: (context) => addData()));
-        },
-        child: Icon(Icons.add, color:Colors.white,),
-        backgroundColor: Colors.grey[400],
       ),
     );
   }
-
-  /*void pickVid() async {
-    videoUrl = videoService.pickVideoFromGallery();
-    initializeVideoPlayer();
-  }
-  void initializeVideoPlayer() {
-    videoPlayerController = VideoPlayerController.file(File(videoUrl!))
-        ..initialize().then((_){
-          setState(() {});
-          videoPlayerController!.play();
-        });
-  }
-
-  Widget videoPreviewWidget() {
-    if(videoPlayerController != null) {
-      return AspectRatio(
-          aspectRatio: videoPlayerController!.value.aspectRatio,
-          child: VideoPlayer(videoPlayerController!)
-      );
-    }else{
-      return CircularProgressIndicator(color: Colors.grey[500]);
-    }
-  }*/
 }
